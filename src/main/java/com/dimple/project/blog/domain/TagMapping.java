b4b22b9503704669c49419c611b2aaff845ef497
pjsqwerty <2416962387@qq.com>
2020-06-12 22:38:41 +0800

    修改博客内容接口

--- src/main/java/com/dimple/project/blog/domain/TagMapping.java
+++ src/main/java/com/dimple/project/blog/domain/TagMapping.java
@@ -17,4 +17,28 @@ public class TagMapping implements Serializable {
     private Long id;
     private Long blogId;
     private Long tagId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getBlogId() {
+        return blogId;
+    }
+
+    public void setBlogId(Long blogId) {
+        this.blogId = blogId;
+    }
+
+    public Long getTagId() {
+        return tagId;
+    }
+
+    public void setTagId(Long tagId) {
+        this.tagId = tagId;
+    }
 }
