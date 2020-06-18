a03913b0f2dc99493e8e5664702fc2fca50fd3b4
pjsqwerty <2416962387@qq.com>
2020-06-18 10:43:46 +0800

    修改广告接口bug

--- src/main/java/com/dimple/project/system/domain/Notice.java
+++ src/main/java/com/dimple/project/system/domain/Notice.java
@@ -47,4 +47,52 @@ public class Notice extends BaseEntity implements Serializable {
      * 公告状态（true正常 false关闭）
      */
     private Boolean status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getHtmlContent() {
+        return htmlContent;
+    }
+
+    public void setHtmlContent(String htmlContent) {
+        this.htmlContent = htmlContent;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
 }
